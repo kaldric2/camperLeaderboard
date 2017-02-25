cb4e6d4e031e7f390d7011049f165e83c52ec3b2
Kevin Aldrich <kevin.aldrich@gmail.com>
2017-02-24 23:11:39 -0800

    added sort to Camper Name column

--- src/_js/main.js
+++ src/_js/main.js
@@ -23,7 +23,7 @@ class LeaderboardHeader extends React.Component {
         return (
             <tr className="header2">
                 <th>Rank</th>
-                <th>Camper Name</th>
+                <th id="camperName" className={(this.props.active === "camperName") ? (this.props.reversed) ? "reversed" : "active" : ""} onClick={this.handleClick}>Camper Name</th>
                 <th id="recent" className={(this.props.active === "recent") ? (this.props.reversed) ? "reversed" : "active" : ""} onClick={this.handleClick}>Recent Points</th>
                 <th id="alltime" className={(this.props.active === "alltime") ? (this.props.reversed) ? "reversed" : "active" : ""} onClick={this.handleClick}>All-time Points</th>
             </tr>
@@ -50,23 +50,52 @@ class Leaderboard extends React.Component {
         super(props);
         this.state = {data: []};
         this.getData = this.getData.bind(this);
+        this.reverseSort = this.reverseSort.bind(this);
+        this.alphaSort = this.alphaSort.bind(this);
+        this.sortColumn = this.sortColumn.bind(this);
 
-        this.getData();
+        this.getData("recent");
     }
 
-    getData(e) { //'recent' or 'alltime'
-        const urlBase = "https://fcctop100.herokuapp.com/api/fccusers/top/";
-        var activeSort = e ? e.target.id : "recent";    
-        var fullURL = urlBase + activeSort;
+    reverseSort() {
+        this.setState({data: this.state.data.reverse(), reversed: !this.state.reversed});
+    }
+
+    alphaSort(activeSort) {
+        this.setState({data: this.state.data.sort((a,b)=>{ 
+                return a.username.localeCompare(b.username);
+                // var nameA = a.username.toLowerCase;
+                // var nameB = b.username.toLowerCase;
+                // if (nameA > nameB) {
+                //     return 1;
+                // } else if (nameA < nameB) {
+                //     return -1;
+                // } else {
+                //     return 0;
+                // }
+            }), active: activeSort, reversed: false});
+    }
+
+    sortColumn(e) {
+        var activeSort = e ? e.target.id : "recent";
 
         if (this.state.active !== undefined && this.state.active === activeSort) {
-            this.setState({data: this.state.data.reverse(), active: activeSort, reversed: !this.state.reversed});
+            this.reverseSort();            
+        } else if (activeSort === "camperName") {
+            this.alphaSort(activeSort);
         } else {
-            $.getJSON(fullURL, (data) => {
-                this.setState({data: data, active: activeSort, reversed: false});
-            });
+            this.getData(activeSort);
         }
         console.log(this.state.data);
+    }
+
+    getData(activeSort) { //'recent' or 'alltime'
+        const urlBase = "https://fcctop100.herokuapp.com/api/fccusers/top/";
+        var fullURL = urlBase + activeSort;
+
+        $.getJSON(fullURL, (data) => {
+            this.setState({data: data, active: activeSort, reversed: false});
+        });
     }
 
     render() {
@@ -81,7 +110,7 @@ class Leaderboard extends React.Component {
                 <table>
                     <thead>
                         <LeaderboardTitle/>
-                        <LeaderboardHeader active={this.state.active} onClick={this.getData} reversed={this.state.reversed} />
+                        <LeaderboardHeader active={this.state.active} onClick={this.sortColumn} reversed={this.state.reversed} />
                     </thead>
                     <tbody>{rows}</tbody>
                     <tfoot><tr colSpan="4" /></tfoot>
